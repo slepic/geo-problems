00f837350769901d3b3f8afd324b2e617ad7d368
slepic <gslepic@gmail.com>
2019-06-15 14:50:19 +0200

    get rid of deg2rad in tests

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -26,12 +26,12 @@ class FormatterTest extends TestCase
 	{
 		return [
 			[
-				new Position(0.0, deg2rad(0.0)),
+				new Position(0.0, 0.0),
 				'%a(%D°%M\'%S" %c), %o',
 				'0°0\'0" N, 0°0\'0" E'
 			],
 			[
-				new Position(deg2rad(-1 * (45 + 15/60 + 25.15 / 3600)), M_PI / 180.0 * (30 + (13 / 60.0) + (40.6 / 3600.0))),
+				Position::fromDegrees(-45 - 15 / 60 - 25.15 / 3600, 30 + (13 / 60.0) + (40.6 / 3600.0)),
 				'%a(%D°%M\'%S.4" %c), %o(%D°%M\'%S.0" %c)',
 				'45°15\'25.15" S, 30°13\'41" E'
 			],
